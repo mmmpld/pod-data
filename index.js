967b0b7e6aa8bc53540bab1b56d895b51593a925
Richard Molloy <richardmolloy@gmail.com>
2023-08-19 19:51:28 +1200

    Add export

---
+++ index.js
@@ -0,0 +1,14 @@
+import aggregate from './json/aggregate.json'
+import localStringsEn from './json/LocaleStringsEn.json'
+import monstats from './json/monstats.json'
+import superUniques from './json/SuperUniques.json'
+import weapons from './json/weapons.json'
+
+export const localStringsEn = localStringsEn
+export const monstats = monstats
+export const superUniques = superUniques
+export const weapons = weapons
+
+export default {
+    aggregate
+}
