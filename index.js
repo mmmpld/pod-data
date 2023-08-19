8f1a878b582d962b3c4f0badcc18dac2e6e9580e
Richard Molloy <richardmolloy@gmail.com>
2023-08-19 20:20:09 +1200

    Fix locale spelling

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import aggregateJson from './json/aggregate.json' assert { type: "json" }
-import localStringsEnJson from './json/LocaleStringsEn.json' assert { type: "json" }
+import localeStringsEnJson from './json/LocaleStringsEn.json' assert { type: "json" }
 import monstatsJson from './json/monstats.json' assert { type: "json" }
 import superUniquesJson from './json/SuperUniques.json' assert { type: "json" }
 import weaponsJson from './json/weapons.json' assert { type: "json" }
 
-export const localStringsEn = localStringsEnJson
+export const localeStringsEn = localeStringsEnJson
 export const monstats = monstatsJson
 export const superUniques = superUniquesJson
 export const weapons = weaponsJson
