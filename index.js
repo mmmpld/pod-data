74dfb35ffe26e123d97e97cf50da7c301c5fe0e2
Richard Molloy <richardmolloy@gmail.com>
2024-09-18 17:37:44 +1200

    Export monLvl

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import localeStringsEnJson from './json/LocaleStringsEn.json' assert { type: "js
 import monstatsJson from './json/monstats.json' assert { type: "json" }
 import superUniquesJson from './json/SuperUniques.json' assert { type: "json" }
 import weaponsJson from './json/weapons.json' assert { type: "json" }
+import monLvlJson from './json/MonLvl.json' assert { type: "json" }
 
 export const localeStringsEn = localeStringsEnJson
 export const monstats = monstatsJson
 export const superUniques = superUniquesJson
 export const weapons = weaponsJson
+export const monLvl = monLvlJson
 
 export default {
     aggregateJson
