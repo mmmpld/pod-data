f6f2871d15e9a5291803d05f5cd9c81783925ddb
Richard Molloy <richardmolloy@gmail.com>
2026-06-27 17:38:16 +1200

    refactor: update JSON import syntax to use "with" keyword

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import aggregateJson from './json/aggregate.json' assert { type: "json" }
-import localeStringsEnJson from './json/LocaleStringsEn.json' assert { type: "json" }
-import monstatsJson from './json/monstats.json' assert { type: "json" }
-import superUniquesJson from './json/SuperUniques.json' assert { type: "json" }
-import weaponsJson from './json/weapons.json' assert { type: "json" }
-import monLvlJson from './json/MonLvl.json' assert { type: "json" }
+import aggregateJson from './json/aggregate.json' with { type: "json" }
+import localeStringsEnJson from './json/LocaleStringsEn.json' with { type: "json" }
+import monstatsJson from './json/monstats.json' with { type: "json" }
+import superUniquesJson from './json/SuperUniques.json' with { type: "json" }
+import weaponsJson from './json/weapons.json' with { type: "json" }
+import monLvlJson from './json/MonLvl.json' with { type: "json" }
 
 export const localeStringsEn = localeStringsEnJson
 export const monstats = monstatsJson
